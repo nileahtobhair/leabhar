f30492a2ebb2c41a52809947dff5c2929f81aab9
Niamh Lawlor <nileahtobhair@gmail.com>
2017-08-15 12:24:10 +0100

    overlay working

--- src/gallery.js
+++ src/gallery.js
@@ -22,78 +22,70 @@ class Gallery extends Component {
       extended_book : undefined
     }
   }
-  extend_book(e,book){
+  extend_book(book){
+    console.log('in extended book '+book);
     this.setState({
       extended : true,
       extended_book : book
     })
   }
 
-  close_extension(){
-    this.setState({
-      extended : false,
-      extended_book : undefined
-    })
-  }
-
-  _render_book_overlay(){
-      var main = this;
-    var extended_book = this.state.extended_book;
-    var jacket = extended_book.jacket_name ? main.state.jackets[extended_book.jacket_name] : undefined;
-  
-    return (
-        <div className='book-overlay'>
-          <div className='cover'>
-            <img src={jacket} alt={"Book Jacket for "+extended_book.name} />
-          </div>
-          <div className='information'>
-            <div className='header'>
-              <div className='lhs'>
-                <div className='name'>{extended_book.name}</div>
-                <div className='author'>{extended_book.author}</div>
-              </div>
-              <div className='rhs'>
-                <div onClick={ (e) => main.close_extension(e) } className='close'>Close</div>
-              </div>
-            </div>
-            <div className='blurb'>{extended_book.blurb}</div>
-            <div className='footer'>
-              <div>Add to ToRead</div>
-              <div>Add to Read</div>
-              <div>Buy on Amazon</div>
-              <div>Rate</div>
-            </div>
-          </div>
-        </div>
-      );
-  }
-  /* 
-  Render a gallery of images imported from json.
-  */
+  /*  Render a gallery of images imported from json file. */
   render() {
     var main = this;
+    var extended_book = this.state.extended_book !== undefined ? this.state.extended_book : undefined;
+    var extended_jacket = this.state.extended_book !== undefined && extended_book.jacket_name ? main.state.jackets[extended_book.jacket_name] : undefined;
+             
     return (
       <span>
         <Header/>
-          {this.state.extended ?
-            <span>
-            { this._render_book_overlay() }
-            </span>
-          : null }
+          
         <div className="gallery">
           
           {books.books.map((book,index) => { 
               var jacket = book.jacket_name ? main.state.jackets[book.jacket_name] : undefined;
+              var extended_book = this.state.extended_book !== undefined ? this.state.extended_book : undefined;
+              var extended_jacket = this.state.extended_book !== undefined && extended_book.jacket_name ? main.state.jackets[extended_book.jacket_name] : undefined;
+             
               return (
-                <div onClick={ (e) => main.extend_book(e,book) } key={'gallery'+index} className='entry'>
-                  { jacket !== undefined ?
-                    <img src={jacket} alt={"Book Jacket for "+book.name} />
-                  : null }
-                </div>
+                <article key={'gallery'+index} className="demo-modal-mixin">
+                  <a onClick={ (e) => main.extend_book(book)} href={"#fade"} className="entry demo-link">
+                    { jacket !== undefined ?
+                      <img src={jacket} alt={"Book Jacket for "+book.name} />
+                    : null }
+                  </a>
+
+                  
+                </article>
               )
             })}
         </div>
-      </span>
+        <dialog className="modal-overlay fade" id={"fade"}>
+          <a href="#" className="demo-btn-close">Close</a>
+          {main.state.extended_book !== undefined ?
+            <div className='book-overlay'>
+              <div className='cover'>
+                <img src={extended_jacket} alt={"Book Jacket for "+extended_book.name} />
+            </div>
+            <div className='information'>
+              <div className='header'>
+                <div className='lhs'>
+                  <div className='name'>{extended_book.name}</div>
+                    <div className='author'>{extended_book.author}</div>
+                  </div>
+                </div>
+                <div className='blurb'>{extended_book.blurb}</div>
+                  <div className='footer'>
+                    <div>Add to ToRead</div>
+                    <div>Add to Read</div>
+                    <div>Buy on Amazon</div>
+                    <div>Rate</div>
+                  </div>
+                </div>
+              </div>
+            : null }
+          </dialog>
+        </span>
     );
   }
 }
