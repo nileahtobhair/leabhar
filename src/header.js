3c3c59fbda2927300dae2418955be4d87f47ac7f
Niamh Lawlor <nileahtobhair@gmail.com>
2017-08-15 14:13:45 +0100

    display search bar

--- src/header.js
+++ src/header.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import './scss/_main.scss';
 import logo from './logo.png'
+import search from './search_icon.svg'
+import close from './search_icon.svg'
 
 class Header extends Component {
 
+   /* Set intial  */
+  constructor(){
+    super();
+    this.state = {
+      show_search : false
+    }
+  }
+
+  toggle_search_bar(){
+    var visible = this.state.show_search ? false: true;
+    this.setState({show_search : visible})
+  }
   update_search_value(){
 
   }
 
   render() {
+    var main = this;
     return (
       <div className="header-menu">
         <ul className='links'>
@@ -16,7 +31,18 @@ class Header extends Component {
           <li><a href='/booker'>Booker</a></li>
           <li><a href='/booker'>Putlizer</a></li>
         </ul>
-        <form className='search'><input type="text" className='search-input'/></form>
+        <form className='search'>
+          {main.state.show_search ? 
+          <div className='search-container'>
+            <img onClick={ (e) => main.toggle_search_bar()} src={search} alt="search icon - show input"/>
+            <input type="text" className='search-input'/>
+          </div>
+          : 
+          <div className='search-container'>
+            <img onClick={ (e) => main.toggle_search_bar()} src={search} alt="close icon - close input"/>
+          </div>
+          }
+        </form>
       </div>
     )
   }
