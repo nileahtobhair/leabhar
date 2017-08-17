258a1ece9356eb4e2debce150db478cd4671b7b0
Niamh Lawlor <nileahtobhair@gmail.com>
2017-08-17 18:54:32 +0100

    commenting + refactoring

--- src/js/List.js
+++ src/js/List.js
@@ -20,16 +20,15 @@ class PrizeList extends Component {
       jackets : images
     }
   }
-
+  /* List all books in prize */
   render() {
     var main = this;
     return (
-      
       <div>
         <Header/>
         <div className="prize">
         <h2 className='prize-title'>The Man Booker Prize for Fiction </h2>
-        <div className='explanation'> The Booker Prize is a literary award , awarded each year for best original novel written in the English language and published in the United Kingdom. The Man Booker Prize was established in 1969. The winner receives £50,000 as well as the £2,500 awarded to each of the shortlisted authors.
+        <div className='explanation'>The Booker Prize is a literary award , awarded each year for best original novel written in the English language and published in the United Kingdom. The Man Booker Prize was established in 1969. The winner receives £50,000 as well as the £2,500 awarded to each of the shortlisted authors.
         Listed below are all the winners of the award since its inception in 1969.</div>
           <div>
               {books.books.map((book,index) => { 
--- src/js/Search.js
+++ src/js/Search.js
@@ -4,10 +4,6 @@ import books from '../books.json';
 
 class Search extends Component {
 
-  /*constructor(props){
-    super();
-   
-  }*/
   constructor(){
     super();
     var images = this.get_jackets(require.context('../jackets', false, /\.(png|jpe?g|svg)$/));
@@ -16,11 +12,13 @@ class Search extends Component {
     }
   }
 
+  /* Load jacket images dynamically */
   get_jackets(r) {
     let images = {};
     r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); return 0; });
     return images;
   }
+
   /* 
    * Search the list of books in the imported json file using filter function.
    * Compare against search term entered by the user and passed to component through props.
@@ -32,46 +30,48 @@ class Search extends Component {
   	let filter_function = (book) =>  {
       	return book.name.toLowerCase().includes(search_term) || book.author.toLowerCase().includes(search_term) || book.blurb.toLowerCase().includes(search_term);
     }
-	let results = books.books.filter(filter_function);
-	return (
-		<div>
-		{results!==undefined && results.length>0 ?
-			<span>
-				{ results.map((book,index) => {
-					var jacket = book.jacket_name ? main.state.jackets[book.jacket_name] : undefined;
-					return(
-						<span className='entry' key={"search-result"+index}>
-							{ jacket !== undefined ?
-		                      <img src={jacket} alt={"Book Jacket for "+book.name} />
-		                    : null }
-		                    <div className='information'>
-		                      <h3 className='title'>{book.name}</h3>
-		                      <div className='author'>{book.author}</div>
-		                      <div>{book.blurb}</div>
-		                    </div>
-						</span>
-					)
-				})}
-			</span>
-		: <p>No results found</p> }
-		</div>
-	)
+  	let results = books.books.filter(filter_function);
+  	return (
+  		<div>
+  		{results!==undefined && results.length>0 ?
+  			<span>
+  				{ results.map((book,index) => {
+  					var jacket = book.jacket_name ? main.state.jackets[book.jacket_name] : undefined;
+  					return(
+  						<span className='entry' key={"search-result"+index}>
+  							{ jacket !== undefined ?
+  		            <img src={jacket} alt={"Book Jacket for "+book.name} />
+  		          : null }
+  		          <div className='information'>
+  		            <h3 className='title'>{book.name}</h3>
+  		            <div className='author'>{book.author}</div>
+  		            <div>{book.blurb}</div>
+  		          </div>
+  						</span>
+  					)
+  				})}
+  			</span>
+  		: <p>No results found</p> }
+  		</div>
+  	)
   }
 
-  /* Main render method  */
+  /* 
+   * Main render method - calls the filter function if search term is not empty or 
+   * has not been passed correctly  
+   */
   render() {
     var main = this;
     return (
     	<span>
     	{ main.props.visible ?
 	    	<div className="search-results">
-	    		 { main.props.term !== undefined && main.props.term !== '' ?
-	    		 <span>
+	    		{ main.props.term !== undefined && main.props.term !== '' ?
+	    		  <span>
 		        	<h1><span className='results-title'>searching : </span>{main.props.term}</h1>
 		        	<span> {main.search_books()} </span>
 		        </span>
-		        	: null
-		    	}
+		      : null }
 	      </div>
       : null }
       </span>
